39230b3bbb22e9532540860ecbaee419edb428e0
web-Nuo <mlocht@outlook.com>
2021-12-04 22:47:42 +0100

    add comments

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 
 
 window.addEventListener("load", function () {
-    const pianoKeys= document.getElementById("piano").contentDocument
-    const pianoWhiteKeysCollection = pianoKeys.getElementsByClassName("white-keys")
-    const pianoWhiteKeys = [...pianoWhiteKeysCollection]
+    const pianoKeys= document.getElementById("piano").contentDocument // since we inserted our SVG into the HTML using the external object element, we have to use contentDocument to access the innards of the SVG file
+    const pianoWhiteKeysCollection = pianoKeys.getElementsByClassName("white-keys") //returns an object containing all white keys
+    const pianoWhiteKeys = [...pianoWhiteKeysCollection] //this object has to be turned into an array so we can access it with the forEach method
     
     pianoWhiteKeys.forEach( key => {
         key.addEventListener("click", function () {
         soundNumber = key.getAttribute("data-audio")
-        console.log(soundNumber)
         let audio = new Audio(`assets/audio/key${soundNumber}.mp3`)
         audio.play()
         })
     });
 
-
+// repeat the same process for the blackkeys
     const pianoBlackKeysCollection = pianoKeys.getElementsByClassName("black-keys")
     const pianoBlackKeys = [...pianoBlackKeysCollection]
 
